ea5482c188c7e9bb6d388c1e66765824db30e123
Nahiyan Alamgir <alnahiyan97@gmail.com>
2017-01-21 10:15:34 +0600

    Patched

--- bin/planitjs.js
+++ bin/planitjs.js
@@ -14,7 +14,7 @@ const path = require('path');
 const planitjs = require('../lib/planitjs');
 
 if (options.version)
-    console.log(clc.blue("Planitjs v.1.0.0. Made by Towni and Eyan."));
+    console.log(clc.blue("Planitjs v.1.0.3. Made by Towni and Eyan."));
 else
     planitjs(fs, clc, path);
     // console.log("I'm towni and I love my eyan!");
